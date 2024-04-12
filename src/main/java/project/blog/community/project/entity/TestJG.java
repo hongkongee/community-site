3a18355b07be191733a79d3e7179766e48f9cfa5
jg <iamjgl@naver.com>
2024-04-12 15:03:32 +0900

    test Commit3 jg

--- src/main/java/project/blog/community/project/entity/TestJG.java
+++ src/main/java/project/blog/community/project/entity/TestJG.java
@@ -3,5 +3,9 @@ package project.blog.community.project.entity;
 public class TestJG {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+
     }
 }

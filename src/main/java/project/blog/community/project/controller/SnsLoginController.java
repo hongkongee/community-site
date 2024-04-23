5cf253b72c32c4f72b50dc82e904b326c8c33d82
minah8 <o_uo_o@naver.com>
2024-04-23 18:22:54 +0900

    네이버 로긴 구현중, 채팅 서버 연결 시도 중

--- src/main/java/project/blog/community/project/controller/SnsLoginController.java
+++ src/main/java/project/blog/community/project/controller/SnsLoginController.java
@@ -6,8 +6,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import project.blog.community.project.service.SnsLoginService;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,6 +30,15 @@ public class SnsLoginController {
     @Value("${sns.kakao.redirect-uri}")
     private String kakaoRidirectUri;
 
+    @Value("${sns.naver.client-id}")
+    private String naverClientId;
+
+    @Value("${sns.naver.client-secret}")
+    private String naverClientSecret;
+
+    @Value("${sns.naver.client-callback-uri}")
+    private String naverCallbakUri;
+
     @GetMapping("/kakao/login")
     public String kakaoLogin() {
         String uri = "https://kauth.kakao.com/oauth/authorize";
@@ -34,7 +49,7 @@ public class SnsLoginController {
         return "redirect:" + uri;
     }
 
-    // 인가 코드 받기
+    // 카카오 인가 코드 받기
     @GetMapping("/auth/kakao")
     public String snsKakao(String code, HttpSession session) {
         log.info("카카오 로그인 인가 코드:{}", code);
@@ -50,6 +65,32 @@ public class SnsLoginController {
 
         // 로그인 처리가 모두 완료되면 홈 화면으로
         return "redirect:/";
+    }
+
+    // 네이버 인가코드 받기
+    @GetMapping("/naver/login")
+    public String naverConnect() {
+        // state용 난수 생성
+        SecureRandom random = new SecureRandom();
+        String state = new BigInteger(130, random).toString(32);
+
+        String uri = "https://nid.naver.com/oauth2.0/authorize?";
+        uri += "response_type=code";
+        uri += "&client_id=" + naverClientId;
+        uri += "&redirect_uri=" + naverCallbakUri;
+        uri += "&state=" + state;
+
+        return "redirect:" + uri;
+
+    }
+
+    // 네이버 토큰 발급 받기
+    @RequestMapping(value = "/callback", method = {RequestMethod.GET, RequestMethod.POST},
+            produces = "application/json")
+    public void naverLogin(@RequestParam(value = "code") String code,
+                           @RequestParam(value = "state") String state) {
+        // 네이버에 요청 보내기
+        RestTemplate template = new RestTemplate();
 
     }
 

63e73b3306d518e30b9f97e223eaff86ca83e68f
mipi1256 <mipi1258@gmail.com>
2024-04-23 10:23:31 +0900

    Min - 서버 오류 수정 완

--- src/main/java/project/blog/community/project/controller/DiaryController.java
+++ src/main/java/project/blog/community/project/controller/DiaryController.java
@@ -1,4 +1,4 @@
-/*
+
 package project.blog.community.project.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -24,14 +24,14 @@ import java.util.List;
 @Slf4j
 public class DiaryController {
 
-    private final BoardService service;
+   private final BoardService service;
 
-    @GetMapping("/diary")
-    public String diary() {
-        log.info("/mypage/diary: GET!!!");
+   @GetMapping("/diary")
+   public String diary() {
+      log.info("/mypage/diary: GET!!!");
 
-        return "mypage/diary";
-    }
+      return "mypage/diary";
+   }
 
 /*
     @GetMapping("/posting_cube")
@@ -55,24 +55,24 @@ public class DiaryController {
     }
 */
 
-    // 큐브 게시물 누르면 그 게시물로 이동
-    @GetMapping("/posting_cube/{bno}")
-    public String detail(@PathVariable("bno") int bno, @ModelAttribute("s") Search search, Model model) {
-        System.out.println("/board/detail: GET! " + bno);
-        BoardDetailResponseDTO dto = service.getDetail(bno);
+   // 큐브 게시물 누르면 그 게시물로 이동
+   @GetMapping("/posting_cube/{bno}")
+   public String detail(@PathVariable("bno") int bno, @ModelAttribute("s") Search search, Model model) {
+      System.out.println("/board/detail: GET! " + bno);
+      BoardDetailResponseDTO dto = service.getDetail(bno);
 
-        model.addAttribute("b", dto);
-        return "home/detail";
-    }
+      model.addAttribute("b", dto);
+      return "home/detail";
+   }
 
-    // 글 작성하기 누르면 새로운 글 작성하는 페이지로 이동
-    @GetMapping("/newposting")
-    public String newposting() {
-        log.info("/mypage/newposting: GET!!!");
+   // 글 작성하기 누르면 새로운 글 작성하는 페이지로 이동
+   @GetMapping("/newposting")
+   public String newposting() {
+      log.info("/mypage/newposting: GET!!!");
 
-        return "mypage/newposting";
-    }
+      return "mypage/newposting";
+   }
 
 
 }
-*/
+
--- src/main/java/project/blog/community/project/service/BoardService.java
+++ src/main/java/project/blog/community/project/service/BoardService.java
@@ -1,6 +1,7 @@
 package project.blog.community.project.service;
 
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -32,180 +33,199 @@ import static project.blog.community.util.LoginUtils.getCurrentLoginMemberAccoun
 @Slf4j
 public class BoardService {
 
-    private final BoardMapper boardMapper;
-    private final UserMapper userMapper;
-    private final LikeMapper likeMapper;
+   private final BoardMapper boardMapper;
+   private final UserMapper userMapper;
+   private final LikeMapper likeMapper;
 
-    // 게시판 목록을 조회
-    public List<BoardListResponseDTO> getList() {
-        List<BoardListResponseDTO> dtoList = new ArrayList<>();
-        List<Board> boardList = boardMapper.findAll("recent",20);
+   // 게시판 목록을 조회
+   public List<BoardListResponseDTO> getList() {
+      List<BoardListResponseDTO> dtoList = new ArrayList<>();
+      List<Board> boardList = boardMapper.findAll("recent", 20);
 
-        for (Board board : boardList) {
-            String nickname = findNickname(board.getWriter()); // writer(account)를 nickname으로 바꾸기
-            BoardListResponseDTO dto = new BoardListResponseDTO(board, nickname);
-            dtoList.add(dto);
-        }
+      for (Board board : boardList) {
+         String nickname = findNickname(board.getWriter()); // writer(account)를 nickname으로 바꾸기
+         BoardListResponseDTO dto = new BoardListResponseDTO(board, nickname);
+         dtoList.add(dto);
+      }
 
-        return dtoList;
+      return dtoList;
 
-    }
+   }
 
-    // 메인화면에서 목록을 조회
-    public List<BoardListResponseDTO> getHotList(String option) {
-        List<BoardListResponseDTO> dtoList = new ArrayList<>();
+   // 메인화면에서 목록을 조회
+   public List<BoardListResponseDTO> getHotList(String option) {
+      List<BoardListResponseDTO> dtoList = new ArrayList<>();
 
-        // 전체 게시판과 달리 좋아요 순으로 6개 게시물만 정렬
-        String type = option;
-        int amount = 6;
-        List<Board> boardList = boardMapper.findAll(type, amount);
+      // 전체 게시판과 달리 좋아요 순으로 6개 게시물만 정렬
+      String type = option;
+      int amount = 6;
+      List<Board> boardList = boardMapper.findAll(type, amount);
 
-        for (Board board : boardList) {
-            String nickname = findNickname(board.getWriter()); // writer(account)를 nickname으로 바꾸기
-            BoardListResponseDTO dto = new BoardListResponseDTO(board, nickname);
-            dtoList.add(dto);
-        }
+      for (Board board : boardList) {
+         String nickname = findNickname(board.getWriter()); // writer(account)를 nickname으로 바꾸기
+         BoardListResponseDTO dto = new BoardListResponseDTO(board, nickname);
+         dtoList.add(dto);
+      }
 
-        return dtoList;
-    }
+      return dtoList;
+   }
 
-    // 카테고리에 따라 다른 게시판 목록을 보여주는 메서드
-    public List<BoardListResponseDTO> getCategoryList(String category) {
-        int amount = 20;
+   // 카테고리에 따라 다른 게시판 목록을 보여주는 메서드
+   public List<BoardListResponseDTO> getCategoryList(String category) {
+      int amount = 20;
 
-        List<BoardListResponseDTO> dtoList = new ArrayList<>();
-        List<Board> boardList = boardMapper.findCategory(category, amount);
-        for (Board board : boardList) {
-            String nickname = findNickname(board.getWriter()); // writer(account)를 nickname으로 바꾸기
-            BoardListResponseDTO dto = new BoardListResponseDTO(board, nickname);
-            dtoList.add(dto);
-        }
+      List<BoardListResponseDTO> dtoList = new ArrayList<>();
+      List<Board> boardList = boardMapper.findCategory(category, amount);
+      for (Board board : boardList) {
+         String nickname = findNickname(board.getWriter()); // writer(account)를 nickname으로 바꾸기
+         BoardListResponseDTO dto = new BoardListResponseDTO(board, nickname);
+         dtoList.add(dto);
+      }
 
-        return dtoList;
+      return dtoList;
 
-    }
+   }
 
-    public BoardDetailResponseDTO getDetail(int bno) {
-        boardMapper.updateViewCount(bno);
+   public BoardDetailResponseDTO getDetail(int bno) {
+      boardMapper.updateViewCount(bno);
 
-        Board board = boardMapper.findOne(bno);
-        String nickname = findNickname(board.getWriter());
+      Board board = boardMapper.findOne(bno);
+      String nickname = findNickname(board.getWriter());
 
-        return new BoardDetailResponseDTO(board, nickname);
+      return new BoardDetailResponseDTO(board, nickname);
 
-    }
+   }
 
-    // account를 주면 nickname을 반환하는 메서드
-    private String findNickname(String account) {
+   // account를 주면 nickname을 반환하는 메서드
+   private String findNickname(String account) {
 
-        try {
-            User user = userMapper.findUser(account);
-            return user.getNickname();
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-            return account;
-        }
-    }
+      try {
+         User user = userMapper.findUser(account);
+         return user.getNickname();
+      } catch (NullPointerException e) {
+         e.printStackTrace();
+         return account;
+      }
+   }
 
 
-    // 게시물의 좋아요 수 바꾸기
-    public int changeLike(LikeRequestDTO dto, HttpServletRequest request) {
-        int bno = dto.getBno();
-        int number = dto.getNumber();
-        
-        // 게시물 테이블의 좋아요 수 업데이트
-        boardMapper.updateLikeCount(bno, number);
+   // 게시물의 좋아요 수 바꾸기
+   // 게시물의 좋아요 수 바꾸기
+   public int changeLike(LikeRequestDTO dto, HttpServletRequest request) {
+      int bno = dto.getBno();
+      int number = dto.getNumber();
 
-        HttpSession session = request.getSession();
-        session.getAttribute("login");
-        // 세션 유틸리티 메서드로 로그인한 유저 ID 가져오기
+      // 게시물 테이블의 좋아요 수 업데이트
+      boardMapper.updateLikeCount(bno, number);
 
-        // 좋아요를 눌렀다면 Like 테이블에 insert
-        if (number > 0) {
+      HttpSession session = request.getSession();
+      session.getAttribute("login");
+      // 세션 유틸리티 메서드로 로그인한 유저 ID 가져오기
+      String currentLoginMemberAccount = getCurrentLoginMemberAccount(session);
 
-            Cookie cookie = new Cookie("like", Integer.toString(bno));
+      // 좋아요를 눌렀다면 Like 테이블에 insert
+      if (number > 0) {
+
+         // 쿠키에 게시글 번호와 로그인 유저 ID 저장
+       /*     Cookie cookie = new Cookie("like" + bno, currentLoginMemberAccount); // ex) "like125", "tjtkdvl"
             cookie.setMaxAge(60);
             cookie.setPath("/");
-            response.addCookie(cookie);
+            response.addCookie(cookie); // 클라이언트에 전송*/
 
-            return 1;
+         // Like 테이블에 로그인 유저 ID와 좋아요 누른 게시글 번호 저장
+         likeMapper.addLike(currentLoginMemberAccount, bno);
 
-        } else { // 좋아요를 안 눌렀다면 해당 게시글에 대한 쿠키 삭제하기
-            Cookie cookie = WebUtils.getCookie(request, "like");
+
+         return 1;
+
+      } else { // 좋아요를 안 눌렀다면 Like 테이블로부터 delete
+
+            /*Cookie cookie = WebUtils.getCookie(request, "like" + bno);
             cookie.setMaxAge(0);
             cookie.setPath("/");
             response.addCookie(cookie);*/
 
-            likeMapper.deleteLike(currentLoginMemberAccount, bno);
+         likeMapper.deleteLike(currentLoginMemberAccount, bno);
 
-            return 0;
-        }
-    }
+         return 0;
+      }
+   }
 
-    public List<BoardMyListResponseDTO> getMyList(HttpServletRequest request, Search page) {
-        List<BoardMyListResponseDTO> myList = new ArrayList<>();
-        List<Board> boardList = boardMapper.findAll(page);
+   // 좋아요를 이전에 눌렀는지 확인 (from tbl_like)
+   public int checkLike(HttpServletRequest request, int bno) {
+      HttpSession session = request.getSession();
+      session.getAttribute("login");
+      // 세션 유틸리티 메서드로 로그인한 유저 ID 가져오기
+      String myAccount = getCurrentLoginMemberAccount(session);
 
-        for (Board board : boardList) {
-            BoardMyListResponseDTO dto = new BoardMyListResponseDTO(board);
-            myList.add(dto);
-        }
-
-        return myList;
-
-    }
-
-    public int getCount(Search page) {
-        return boardMapper.getCount(page);
-    }
-
-    public List<BoardMyListResponseDTO> getMyList(Search page, HttpServletRequest request) {
-
-        HttpSession session = request.getSession();
-        LoginUserResponseDTO loginDto = (LoginUserResponseDTO) session.getAttribute("login");
-        log.info("dto: {}", loginDto);
-
-        String currentLoginMemberAccount = getCurrentLoginMemberAccount(session);
-
-        List<BoardMyListResponseDTO> myList = new ArrayList<>();
-        List<Board> boardList = boardMapper.findMine(page, currentLoginMemberAccount);
-        for (Board board : boardList) {
-            BoardMyListResponseDTO dto = new BoardMyListResponseDTO(board);
-            myList.add(dto);
-        }
-        return myList;
-    }
+      int likeCount = likeMapper.checkLike(myAccount, bno);
+      return likeCount;
+   }
 
 
-    // 게시글 업로드
-    public void saveBoard(String category, String title, String content, String filePath, String writer) {
+   public List<BoardMyListResponseDTO> getMyList(HttpServletRequest request, Search page) {
+      List<BoardMyListResponseDTO> myList = new ArrayList<>();
+      List<Board> boardList = boardMapper.findAll(page);
+
+      for (Board board : boardList) {
+         BoardMyListResponseDTO dto = new BoardMyListResponseDTO(board);
+         myList.add(dto);
+      }
+
+      return myList;
+
+   }
+
+   public int getCount(Search page) {
+      return boardMapper.getCount(page);
+   }
+
+   public List<BoardMyListResponseDTO> getMyList(Search page, HttpServletRequest request) {
+
+      HttpSession session = request.getSession();
+      LoginUserResponseDTO loginDto = (LoginUserResponseDTO) session.getAttribute("login");
+      log.info("dto: {}", loginDto);
+
+      String currentLoginMemberAccount = getCurrentLoginMemberAccount(session);
+
+      List<BoardMyListResponseDTO> myList = new ArrayList<>();
+      List<Board> boardList = boardMapper.findMine(page, currentLoginMemberAccount);
+      for (Board board : boardList) {
+         BoardMyListResponseDTO dto = new BoardMyListResponseDTO(board);
+         myList.add(dto);
+      }
+      return myList;
+   }
 
 
-        Board uploadedBoard = Board.builder()
-                .category(stringToCategory(category))
-                .title(title)
-                .content(content)
-                .writer(writer)
-                .postImg(filePath)
-                .build();
+   // 게시글 업로드
+   public void saveBoard(String category, String title, String content, String filePath, String writer) {
 
 
-        boardMapper.save(uploadedBoard);
-    }
+      Board uploadedBoard = Board.builder()
+            .category(stringToCategory(category))
+            .title(title)
+            .content(content)
+            .writer(writer)
+            .postImg(filePath)
+            .build();
 
-    // 문자열을 Category 타입으로 바꾸는 메서드
-    private Category stringToCategory(String str) {
-        String upperCategory = str.toUpperCase();
-        return Category.valueOf(upperCategory);
 
-    }
+      boardMapper.save(uploadedBoard);
+   }
 
-    public String stringToCategoryDescription(String str) { // str = "movie"
-        String upperCategory = str.toUpperCase(); // upperCategory = "MOVIE"
-        Category category = Category.valueOf(upperCategory); // category = Category.MOVIE
-        return category.getDescription(); // return value = "영화글"
-    }
+   // 문자열을 Category 타입으로 바꾸는 메서드
+   private Category stringToCategory(String str) {
+      String upperCategory = str.toUpperCase();
+      return Category.valueOf(upperCategory);
 
+   }
+
+   public String stringToCategoryDescription(String str) { // str = "movie"
+      String upperCategory = str.toUpperCase(); // upperCategory = "MOVIE"
+      Category category = Category.valueOf(upperCategory); // category = Category.MOVIE
+      return category.getDescription(); // return value = "영화글"
+   }
 
 
 }

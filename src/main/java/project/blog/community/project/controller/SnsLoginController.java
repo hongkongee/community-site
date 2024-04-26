befeda20a1ddb2321d6fdbf33b2dd495469a0de5
minah8 <o_uo_o@naver.com>
2024-04-26 15:37:42 +0900

    소셜 로그인 사진 렌더링 완료

--- src/main/java/project/blog/community/project/controller/SnsLoginController.java
+++ src/main/java/project/blog/community/project/controller/SnsLoginController.java
@@ -66,7 +66,7 @@ public class SnsLoginController {
         snsLoginService.kakaoLogin(params, session);
 
         // 로그인 처리가 모두 완료되면 홈 화면으로
-        return "redirect:/";
+        return "redirect:/home/main";
     }
 
     // 네이버 인가코드 받기
@@ -100,71 +100,8 @@ public class SnsLoginController {
 
         snsLoginService.naverLogin(params, session);
 
-        return "redirect:/";
+        return "redirect:/home/main";
 
     }
-
-//    // 네이버 토큰 발급 받기
-//    @RequestMapping(value = "/auth/naver", method = {RequestMethod.GET, RequestMethod.POST},
-//            produces = "application/json")
-//    public void naverLogin(@RequestParam(value = "code") String code,
-//                           @RequestParam(value = "state") String state) {
-//        // 네이버에 요청 보내기
-//        WebClient webClient = WebClient.builder()
-//                .baseUrl("https://nid.naver.com")
-//                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-//                .build();
-//
-////        HttpHeaders headers = new HttpHeaders();
-//
-//        JSONObject response = webClient.post()
-//                .uri(uriBuilder -> uriBuilder
-//                        .path("/oauth2.0/token")
-//                        .queryParam("client_id", naverClientId)
-//                        .queryParam("client_secret", naverClientSecret)
-//                        .queryParam("grant_type", "authorization_code")
-//                        .queryParam("state", state)
-//                        .queryParam("code", code)
-//                        .build())
-//                .retrieve().bodyToMono(JSONObject.class)
-//                .subscribe(
-//                        ResponseBody -> {
-//                            JSONObject jsonObject = new JSONObject();
-//                            String token = jsonObject.toString();
-//                        }
-//                )
-//
-//
-//        // 네이버에서 온 응답에서 토큰을 추출
-//        String token = (String) response.get("access_token");
-//        getUserInfo(token);
-//        log.info(token);
-//
-//    }
-//
-//    private String getUserInfo(String accessToken) {
-//        // 사용자 정보 요청하기
-//        WebClient webClient = WebClient.builder()
-//                .baseUrl("https://openapi.naver.com")
-//                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-//                .build();
-//
-//        JSONObject reponse = webClient.get()
-//                .uri(uriBuilder -> uriBuilder
-//                        .path("/v1/nid/me")
-//                        .build())
-//                .header("Authorizaion", "Bearer " + accessToken)
-//                .retrieve()
-//                .bodyToMono(JSONObject.class).block();
-//
-//        // 원하는 정보 추출하기
-//        Map<String, Object> res = (Map<String, Object>) reponse.get("response");
-//        String id = (String) res.get("id");
-//
-//        log.info(accessToken);
-//        log.info(res.toString());
-//
-//        return "home/main";
-//    }
 
 }

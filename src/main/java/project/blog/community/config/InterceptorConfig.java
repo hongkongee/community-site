4a850e9bf7734046e01f09e7156e1b779ed901b8
minah8 <o_uo_o@naver.com>
2024-04-28 13:47:34 +0900

    채팅창 진입 시 인터셉터 추가

--- src/main/java/project/blog/community/config/InterceptorConfig.java
+++ src/main/java/project/blog/community/config/InterceptorConfig.java
@@ -25,7 +25,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
 
       registry
             .addInterceptor(boardInterceptor)
-            .addPathPatterns("/home/**", "/market/**", "/game/**")
+            .addPathPatterns("/home/**", "/market/**", "/game/**", "/chat/chat")
             .excludePathPatterns("/home/main");
 
       registry
